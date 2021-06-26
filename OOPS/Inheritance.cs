328e976135f3ccc106e0b7967edf06d344d5187f
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-26 10:26:40 +0530

    OOPS with inheritance

---
+++ OOPS/Inheritance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class Inheritance
+    {
+        public virtual int addOrMultiply(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+
+    }
+    class Multiplication : Inheritance
+    {
+        public override int addOrMultiply(int number1,int number2)
+        {
+            return number2 * number1;
+        }
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inheritance inheritance = new Multiplication();
+            Console.WriteLine(inheritance.addOrMultiply(5, 3));
+            inheritance = new Inheritance();
+            inheritance.addOrMultiply(3,5);
+            
+           
+
+
+            
+        }
+    }
+}
